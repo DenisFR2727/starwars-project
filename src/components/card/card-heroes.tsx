7a6375241830816fb4f7e4d7baa4d0c65b2ac213
Denis Leon <dh92fr@ukr.net>
2025-11-07 01:22:46 +0200

    create card hero, add module styles

--- src/components/card/card-heroes.tsx
+++ src/components/card/card-heroes.tsx
@@ -1,3 +1,22 @@
-export default function HeroCard() {
-  return <div className="hero_card"></div>;
+import type { Hero } from "../../api/types";
+import styles from "./card-heroes.module.scss";
+
+interface HeroProps {
+  hero: Hero;
+}
+export default function HeroCard({ hero }: HeroProps) {
+  return (
+    <div className={styles.hero_card}>
+      <img
+        className={styles.hero_img}
+        src="/dart_veider.jpg"
+        alt="card_img"
+        width={250}
+        height={300}
+      />
+      <div className={styles.hero_name}>
+        <p>{hero.name}</p>
+      </div>
+    </div>
+  );
 }
--- src/components/heroes-list/heroes-list.tsx
+++ src/components/heroes-list/heroes-list.tsx
@@ -1,6 +1,9 @@
 import useFetchHeroes from "../../hooks/useFetchHeroes";
+import HeroCard from "../card/card-heroes";
 import Error from "../ui/error";
 import Loader from "../ui/loader";
+
+import styles from "./heroes-list.module.scss";
 
 export default function HeroesList() {
   const { loading, heroes, error } = useFetchHeroes();
@@ -9,12 +12,10 @@ export default function HeroesList() {
   if (error) return <Error />;
 
   return (
-    <div>
-      <ul>
-        {heroes.map((hero) => (
-          <li key={hero.name}>{hero.name}</li>
-        ))}
-      </ul>
+    <div className={styles.heroes_list}>
+      {heroes.map((hero) => (
+        <HeroCard key={hero.id} hero={hero} />
+      ))}
     </div>
   );
 }
